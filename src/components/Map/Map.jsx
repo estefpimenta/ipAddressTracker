ffe363f22905f1d897190333cc8515e290f6b4ab
estefanno pimenta <estefanno.pimenta@gmail.com>
2026-05-18 09:44:09 -0300

    React leaflet integration

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,12 +1,34 @@
 import './Map.css'
 
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 function Map() {
 
+    const position = [-23.5275, -46.6315]
 
     return(
 
         <section className="map">
-            <p>Mapa aqui</p>
+            <MapContainer
+                center={position}
+                zoom={12}
+                scrollWheelZoom={true}
+            >
+
+                <TileLayer
+                    attribution='&copy; OpenStreetMap contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+
+                <Marker position={position}>
+
+                <Popup>
+                    Localização atual
+                </Popup>
+                
+                </Marker>
+
+            </MapContainer>
         </section>
 
     )
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
 
+import 'leaflet/dist/leaflet.css'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
