7c4dc5bda3d88c5a69a3814a203e00d010ef9589
estefanno pimenta <estefanno.pimenta@gmail.com>
2026-05-19 10:10:04 -0300

    immersive weather function - UX/UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,27 +2,46 @@ import './Header.css';
 
 import SearchBar from '../SearchBar/SearchBar';
 import InfoPanel from '../InfoPanel/InfoPanel';
+import dayVideo from '../../assets/videos/video-bg-day.mp4';
+import nightVideo from '../../assets/videos/video-bg-night.mp4';
 
 function Header({ ipData, fetchIPData, isLoading, error, weatherData }) {
 
 
+    const isNight = weatherData && !weatherData.isDay
+
+
     return(
         <header className='header'>
-            <h1>Rastreador de IP</h1>
-            <SearchBar 
-                fetchIPData={fetchIPData} 
-                isLoading={isLoading} />
 
-            {error && (
-                <div className="error-message">
-                    {error}
-                </div>
-            )}
+            <video autoPlay muted loop playsInline className='header-video' key={isNight ? 'night' : 'day'}>
+                <source src={isNight ? nightVideo : dayVideo} type="video/mp4" />
+            </video>
 
-            <InfoPanel 
-                ipData={ipData} 
-                isLoading={isLoading} 
-                weatherData={weatherData} />
+            <div className="header-content">
+
+                <h1>Rastreador de IP</h1>
+
+                <SearchBar 
+                    fetchIPData={fetchIPData} 
+                    isLoading={isLoading}
+                />
+
+                {error && (
+                    <div className="error-message">
+                        {error}
+                    </div>
+                )}
+
+                <InfoPanel 
+                    ipData={ipData} 
+                    isLoading={isLoading} 
+                    weatherData={weatherData}
+                />
+
+            </div>
+
+           
         </header>
     )
 }
